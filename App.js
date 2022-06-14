82dce74cd73f1bfb79ef4e5dea26173cdcd8e303
Janeth1513 <94811417+Janeth1513@users.noreply.github.com>
2022-06-13 18:20:48 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {ProfileScreen} from './src/components/ProfileScreen';
+import {HomeScreen} from './src/components/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        name="Home"
+          component={HomeScreen}
+          options={{ title: 'BienVenidos' }}>
+          </Stack.Screen> 
+          
+        
+
+        <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        ></Stack.Screen> 
+
+
+      
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
---
+++ src/components/ProfileScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native'
+const ProfileScreen = ({navigation, route}) => {
+  return (
+    <Text>El perfil del usuario es: {route.params.name}</Text>
+  )
+}
+
+export default ProfileScreen
